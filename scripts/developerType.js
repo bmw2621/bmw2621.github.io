2547d27316f12f8bbf288e49149afd4ef4961f4b
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-10-30 12:13:58 -0400

    Add typing animation

---
+++ scripts/developerType.js
@@ -0,0 +1,30 @@
+var words = document.getElementById("eegg");
+var titles = ["Web Developer", "Data Analyst", "Programmer","3D Artist", "Photographer","Homebrewer"]
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+async function addText(title) {
+  for( let j = 0; j < title.length; j++){
+    words.innerHTML += title.charAt(j)
+    await sleep(150)
+  }
+
+  await sleep(2000)
+
+  let wordsText = words.innerHTML
+  for (let i = wordsText.length - 1; i >= 0; i--) {
+    words.innerHTML = wordsText.slice(0, i)
+    await sleep(100)
+  }
+}
+
+async function addAllText() {
+  for(let idx = 0; idx < titles.length; idx++){
+    await sleep(5500).then(addText(titles[idx]))
+  }
+}
+
+addAllText()
+setInterval(() => addAllText(), (5500 * titles.length) + 100)
