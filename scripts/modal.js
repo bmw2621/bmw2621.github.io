a5b1051b1add80d40813075acf1f0c62d3437802
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-11-01 14:37:36 -0400

    updates to modal functionality on art page

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,33 +1,30 @@
  function displayModal(image) {
     // Get the modal
-    var modal = document.getElementById("myModal");
     var modalContent = document.getElementById("modal-content");
+    var modal = document.getElementById("modal");
 
-    var img = new Image()
+    //Get width of image passed in as a paratmeter
+    var img = new Image();
     img.src=image;
-    imgWidth = img.width
-    console.log(imgWidth)
-    modalContent.style.width = imgWidth + "px"
+    imgWidth = img.width;
+    console.log(imgWidth);
 
-    // Get the <span> element that closes the modal
-    
-    modalContent.insertAdjacentHTML("beforeend", "<span class='close'>&times;</span>")
-    var span = document.getElementsByClassName("close")[0];
-    modalContent.insertAdjacentHTML("beforeend", "<img class='photo' src='" + image + "'>")
+    //Set modal width to width of image
+    modalContent.style.width = imgWidth + "px";
+    modalContent.style.maxWidth = "90%";
+
+    // Build modal content and display
+    modalContent.insertAdjacentHTML("beforeend", "<span id='closeBtn' class='close'>&times;</span>") //Add close button
+    var closeBtn = document.getElementById("closeBtn"); 
+    modalContent.insertAdjacentHTML("beforeend", `<img class='photo' intrensicsize='${img.width} x ${img.height}' src='${image}'>`)
+    modalContent.style.display = "block";
     modal.style.display = "block";
 
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    closeBtn.onclick = () => {
         modalContent.innerHTML = ""
+        modalContent.style.display = "none"
         modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modalContent.innerHTML = ""
-            modal.style.display = "none";
-        }
     }
  }
