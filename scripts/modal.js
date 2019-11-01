d7e66869ace66125d64c1a0ad389e2b8873e34f6
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-11-01 11:36:48 -0400

    fix modal closeout action

--- scripts/modal.js
+++ scripts/modal.js
@@ -26,7 +26,8 @@
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
-         modal.style.display = "none";
+            modalContent.innerHTML = ""
+            modal.style.display = "none";
         }
     }
  }
